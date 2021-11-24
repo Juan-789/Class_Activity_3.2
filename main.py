8b76dbb6643dd7b5fc07116e66c3c73216591f6a
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-11-24 08:38:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 2. Write a program that asks a user for an integer, print "YES" if it's positive and print "NO" otherwise.
 """
-unknownNumber = int(input("Type in a number and I will determine wether it's negative or positive. "))
-if unknownNumber >= 0:
+unknownNumber = int(input("Type in a number and I will determine wether it's negative, positive, or zero. "))
+if unknownNumber > 0:
   print ("It's positive.")
+elif unknownNumber == 0:
+  print ("It's zero")
 else:
   print("It's negative.")
 #Compares the numbers to see if they are above or under zero
